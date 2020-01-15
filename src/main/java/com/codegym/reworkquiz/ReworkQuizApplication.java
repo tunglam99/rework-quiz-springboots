8e328d1120bbb1c94b3f58c86e2d41da20e7974d
tunglam99 <lamnthnue.edu.vn@gmail.com>
2020-01-15 16:16:10 +0700

    config port, hibernate,databse

---
+++ src/main/java/com/codegym/reworkquiz/ReworkQuizApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.reworkquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReworkQuizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReworkQuizApplication.class, args);
+    }
+
+}
